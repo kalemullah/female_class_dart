8ebd41457c49c2eb6b383bb1599baf92bb7248ad
kaleem ullah <51668264+kalemullah@users.noreply.github.com>
2024-10-27 13:18:49 +0500

    local db

--- localdb/lib/main.dart
+++ localdb/lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:get_storage/get_storage.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
-void main() {
+void main() async{
+  await GetStorage.init();
   runApp(const MyApp());
 }
 
