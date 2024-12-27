b5e373aadb2f83b0cd22c78a241bcba6f39de4a0
kaleem ullah <51668264+kalemullah@users.noreply.github.com>
2024-12-28 03:05:45 +0500

    weather app

--- statmanagment/lib/country/country_provider.dart
+++ statmanagment/lib/country/country_provider.dart
@@ -7,10 +7,12 @@ class CountryProvider extends ChangeNotifier {
   CountryProvider() {
     pickCountry();
   }
-
+  bool isloader = false;
   CountryNamAndFlageModel countryName = CountryNamAndFlageModel();
 
   pickCountry() async {
+    isloader = true;
+    notifyListeners();
     var respos = await http.get(
         Uri.parse('https://countriesnow.space/api/v0.1/countries/flag/images'));
     // print(" ${respos.body}");
@@ -20,5 +22,7 @@ class CountryProvider extends ChangeNotifier {
       notifyListeners();
       print('this is tempdata ${tempdata}');
     }
+    isloader = false;
+    notifyListeners();
   }
 }
--- statmanagment/lib/country/country_screen.dart
+++ statmanagment/lib/country/country_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 import 'package:provider/provider.dart';
 import 'package:statmanagment/country/country_provider.dart';
 
@@ -16,22 +17,50 @@ class _CountryScreenState extends State<CountryScreen> {
       create: (context) => CountryProvider(),
       child: Consumer<CountryProvider>(
         builder: (context, model, child) => Scaffold(
-          body: Column(
-            children: [
-              SizedBox(
-                height: 200,
-              ),
-              Text('this is data ${model.countryName.data?[0].name}'),
-              SizedBox(
-                height: 20,
-              ),
-              Text(' ${model.countryName.data?[0].flag}'),
-              model.countryName.data?[0].flag == null
-                  ? CircularProgressIndicator()
-                  : Image.network(
-                      model.countryName.data![2].flag!,
-                    )
-            ],
+          body: Center(
+            child: Column(
+              children: [
+                const SizedBox(height: 100),
+                Text('Country Name and falge'),
+                const SizedBox(height: 20),
+                Expanded(
+                  child: ListView.builder(itemBuilder: (context, index) {
+                    return Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Card(
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text('${model.countryName.data?[index].name}'),
+                            Container(
+                              height: 50,
+                              width: 100,
+                              child: SvgPicture.network(
+                                model.countryName.data![index].flag!,
+                                height: 50,
+                                width: 100,
+                                fit: BoxFit.fill,
+                              ),
+                            )
+                          ],
+                        ),
+                      ),
+                    );
+                  }),
+                )
+                // Text('${model.countryName.data?[0].name}'),
+                // const SizedBox(height: 20),
+                // model.countryName.data?[0].flag == null
+                //     ? const CircularProgressIndicator()
+                //     :
+                //
+                // SvgPicture.network(
+                //         model.countryName.data![0].flag!,
+                //         height: 100,
+                //         width: 100,
+                //       )
+              ],
+            ),
           ),
         ),
       ),
